88d13ccd64f3aa632a3cd129fc0fd2f37f1c2618
user <user@users-iMac.local>
2019-03-09 16:56:01 +0000

    set up header footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <p>Random Quote Machine developed with love by Aimee Tacchi</p>
+      </div>
+    )
+  }
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+        <header id="text">
+		    <img class="icon" src="http://simpleicon.com/wp-content/uploads/roll-film.svg"/>
+	        <h1 >Random Quote Machine! :D</h1>
+	    </header>
+    )
+  }
+}
